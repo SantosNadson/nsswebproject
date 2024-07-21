6e880071b82d537da124faeab46fb48617a82c47
Nadson <nadsonsantossilva17@gmail.com>
2024-07-21 18:23:12 -0300

    readjust mobile breakpoints

---
+++ homepage/script.js
@@ -0,0 +1,12 @@
+function exitAdvice1(){
+    alert("Você será levado para o site de hospedagens ALL")
+}
+function exitAdvice2(){
+    alert("Você será levado para o site de viagens LATAM AIRLINES")
+}
+function mobileAdvice1(){
+    alert("Você será levado para o site de hospedagens ALL")
+}
+function mobileAdvice2(){
+    alert("Você será levado para o site de viagens LATAM AIRLINES")
+}
